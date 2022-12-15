cf2b086eed639f9353fa4ec68cf30d30883384df
umutasmaz <76012751+umutasmaz@users.noreply.github.com>
2022-12-15 08:48:03 +0300

    Add files via upload

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Umut Asmaz on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
